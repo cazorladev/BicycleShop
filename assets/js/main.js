816c53c9f122e04ee2dffdbe7614fa4c337034d4
cazorladev <cazorladevnet@gmail.com>
2025-01-19 18:36:36 -0500

    Navbar Avance 2

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const menuToggle = document.querySelector(".menu-toggle");
+//   const navMenu = document.querySelector(".nav-menu");
+
+//   menuToggle.addEventListener("click", () => {
+//     navMenu.classList.toggle("active");
+//   });
+// });
+
+
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  document.querySelector('.nav-menu').classList.toggle('show');
+});
