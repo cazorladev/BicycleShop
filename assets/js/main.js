1c78d1fdefc166cb3954ef7a9078e24618b42811
vienheno <herrera.victor@outlook.com>
2025-01-26 18:42:34 -0500

    Rescatando el index

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,16 +14,4 @@ document.querySelector('.menu-toggle').addEventListener('click', () => {
 
 const carrusel = document.querySelector('.carrusel');
 const clone = carrusel.innerHTML;
-carrusel.innerHTML += clone;// document.addEventListener("DOMContentLoaded", () => {
-//   const menuToggle = document.querySelector(".menu-toggle");
-//   const navMenu = document.querySelector(".nav-menu");
-
-//   menuToggle.addEventListener("click", () => {
-//     navMenu.classList.toggle("active");
-//   });
-// });
-
-
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-  document.querySelector('.nav-menu').classList.toggle('show');
-});
+carrusel.innerHTML += clone;
